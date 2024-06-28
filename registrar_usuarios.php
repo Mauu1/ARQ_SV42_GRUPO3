26832e3dcb5f39fc0e3bb630088f480558019a04
playboimau <137224099+Mauu1@users.noreply.github.com>
2024-06-27 23:04:55 -0500

    Add files via upload

---
+++ registrar_usuarios.php
@@ -0,0 +1,30 @@
+<?php
+$user = "GrupoArqui";  // Cambia a tu usuario de MySQL
+$pass = "123";      // Cambia a tu contraseña de MySQL
+$host = "localhost";
+
+$connection = mysqli_connect($host, $user, $pass, "Reservas_Teleticket");
+
+if (!$connection) {
+    die("No se ha podido conectar con el servidor: " . mysqli_connect_error());
+}
+
+$nombre = $_POST["nombre"];
+$apellido = $_POST["apellido"];
+$correo = $_POST["correo"];
+$fecha_nacimiento = $_POST["fecha_nacimiento"];
+
+$insertar = "INSERT INTO usuarios (nombre, apellido, correo, fecha_nacimiento) VALUES ('$nombre', '$apellido', '$correo', '$fecha_nacimiento')";
+$resultado = mysqli_query($connection, $insertar);
+
+if (!$resultado) {
+    die("Error al registrar usuario: " . mysqli_error($connection));
+} else {
+    echo "Usuario registrado exitosamente.";
+}
+
+mysqli_close($connection);
+
+header("Location: index.html");
+exit();
+?>
